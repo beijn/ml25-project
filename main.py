1c1f6f68ed0507d7e7cb60f98e25ff198f2e01fc
beijn <105940600+beijn@users.noreply.github.com>
2025-07-09 17:44:59 +0200

    integrated alinas simplified dataloading

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 # %% 
-from preprocess_data import preprocess_data
+from preprocess_data import load_and_preprocess_data
 
-# TODO Beni yayyy
+import torch
+from torch.utils.data import TensorDataset, DataLoader, random_split
+
+# Peprocess Data
+coords, prices, coord_scaler, price_scaler = load_and_preprocess_data()
+
+# Create Dataloaders
+_T = lambda a: torch.tensor(a, dtype=torch.float32)
+dataset = TensorDataset(_T(coords), _T(prices))
+
+train_dl, val_dl, test_dl = [DataLoader(ds, batch_size=128) for ds in 
+                             random_split(dataset, [0.7, 0.15, 0.15])]
+
+# Print Test Output
+for coords_batch, prices_batch in train_dl:
+    print("Coordinates:", coords_batch.shape)
+    print("Prices:", prices_batch.shape)
+    break
--- preprocess_data.py
+++ preprocess_data.py
@@ -1,11 +1,20 @@
-# TODO Alina yayyy
-# you have to run functions defined here from the idea2_priceMap.py file so that the relative imports work 
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
 
 from download_files import download_and_extract, listings_url
 
-
-def preprocess_data():
+def load_and_preprocess_data():
   csv_path = download_and_extract(listings_url)
+  df = pd.read_csv(csv_path)[['latitude', 'longitude', 'price']].dropna()
+    
+  coords = df[['latitude', 'longitude']].values.astype(np.float32)
+  prices = df['price'].apply(lambda s: s.removeprefix('$').replace(',','')).values.astype(np.float32).reshape((-1,1))
 
-  # ... 
+  coord_scaler = MinMaxScaler()
+  price_scaler = MinMaxScaler()
   
+  coords_scaled = coord_scaler.fit_transform(coords)
+  prices_scaled = price_scaler.fit_transform(prices)
+
+  return coords_scaled, prices_scaled, coord_scaler, price_scaler
